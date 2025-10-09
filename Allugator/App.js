62f5ad0cf751b0ec1c4b769e0dafd62718aeac25
paulo <paulogabrielpk0@gmail.com>
2025-10-09 18:29:14 -0300

    :recycle: refactor(App.js): atualizado a estrutura do app

--- Allugator/App.js
+++ Allugator/App.js
@@ -7,10 +7,12 @@ import { StatusBar } from 'expo-status-bar';
 import AuthScreen from './screens/auth/AuthScreen';
 import LoginScreen from './screens/login/LoginScreen';
 import RegisterScreen from './screens/register/RegisterScreen';
+import ForgotPasswordScreen from './screens/forgotpassword/ForgotPasswordScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  console.log('App rendering...');
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
@@ -24,6 +26,7 @@ export default function App() {
         <Stack.Screen name="Auth" component={AuthScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
