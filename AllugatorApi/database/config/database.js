99e2b8d5536d4591b78c1e803f166d55f6e0b33c
Luan T. Felix <lutavares.bros@gmail.com>
2025-11-24 11:57:17 -0300

    :wrench: chore(database.js): adiciona criação de database caso não exista

--- AllugatorApi/database/config/database.js
+++ AllugatorApi/database/config/database.js
@@ -1,7 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
 const dbPath = path.resolve(__dirname, '../data/database.sqlite');
+
+if (!fs.existsSync(path.dirname(dbPath))) {
+    console.log('Criando banco de dados SQLite.')
+    const dbDir = path.dirname(dbPath);
+    if (!fs.existsSync(dbDir)) {
+        fs.mkdirSync(dbDir, { recursive: true });
+        fs.writeFileSync(dbPath, '');
+        console.log('Banco de dados criado!')
+    }
+}
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
